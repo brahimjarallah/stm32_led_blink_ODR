337786944b0b5d71dd69b04ed6a007c1bdc123a6
Brahim Jarallah <brahim.jarallah@gmail.com>
2022-05-20 19:23:15 +0100

    Update main.c

--- main.c
+++ main.c
@@ -7,7 +7,7 @@ void delay(void);
 int main(){
 	
 	RCC->AHB1ENR |= 1000; // enable PORTD clock  - 1bit/digit 16bits
-	GPIOD->MODER |= 0x40000000; // make pin D as output
+	GPIOD->MODER |= 0x40000000; // make pin 15 as output (led LD6 PD15)
 	// enable MODR for GPIOA 4bits/digit (2bits-inputs for MODR) 
 	// (pin 0 to pin 15 "2bits/pin")   32 bits
 	// 01 00 - 00 00 - 00 00  -  00 00 		- 00 00 -  00 00 - 00 00 - 00 00 
